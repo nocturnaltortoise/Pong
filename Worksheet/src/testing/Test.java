a50265e0f40813501c19e7a92f39c535df6fe27e
nocturnaltortoise <simonturner34@gmail.com>
2014-02-25 22:54:02 +0000

    Setting up, adding console calculator kit, some testing projects.

---
+++ Worksheet/src/testing/Test.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class Test {
+	
+	public static void main(String[] args){
+		String test = "Hello world!";
+		String testTwo = "Hello world";
+		
+		int comparison = test.compareTo(testTwo);
+		
+		System.out.print(comparison);
+	}
+	
+}
