0692db2968087f2234d19e4819cc956c30440fca
nocturnaltortoise <simonturner34@gmail.com>
2014-03-29 00:23:46 +0000

    adding comments

--- PongMK5000/src/game/Game.java
+++ PongMK5000/src/game/Game.java
@@ -25,18 +25,18 @@ import javax.swing.Timer;
 public class Game extends JPanel implements ActionListener, KeyListener {
 
 	private static final long serialVersionUID = 1L;
-	
-	//game state variables for pausing the game and displaying score
+
+	// game states
 	private final int GAME_PLAYING = 1;
 	private final int GAME_PAUSED = 0;
 	private final int GAME_ENDED = 2;
 	private int gameState = GAME_PAUSED;
 
-	//width and height of the game window
+	// window height and width
 	private final int WIDTH = 640;
 	private final int HEIGHT = 480;
 
-	//paddle constants - starting positions, dimensions and movement speed
+	// paddle constants
 	private final int PADDLE_WIDTH = 16;
 	private final int PADDLE_HEIGHT = 72;
 	private final int PADDLE_START_Y = (HEIGHT - PADDLE_HEIGHT) / 2;
@@ -44,38 +44,51 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 	private final int PADDLE2_START_X = WIDTH - (PADDLE_WIDTH * 2);
 	private final int PADDLE_SPEED = 8;
 
-	//ball constants
+	// ball constants
 	private final int BALL_DIAMETER = 8;
 	private final int BALL_START_X = (WIDTH - BALL_DIAMETER) / 2;
 	private final int BALL_START_Y = (HEIGHT - BALL_DIAMETER) / 2;
-	
-	//ball variables - higher xvelocity than yvelocity means that the ball travels at an angle
-	private double ballXVelocity = 3;
-	private double ballYVelocity= 2.5;
 
-	//score variables
+	// ball variables - higher xvelocity than yvelocity means that the ball
+	// travels at an angle
+	private double ballXVelocity = 3;
+	private double ballYVelocity = 2.5;
+
+	// score variables
 	private int p1score = 0;
 	private int p2score = 0;
 
-	//rectangle objects for the paddles, the game background and the "ball"
-	Rectangle ball = new Rectangle(BALL_START_X, BALL_START_Y, BALL_DIAMETER, BALL_DIAMETER);
+	// rectangle objects for the paddles, the game background and the "ball"
+	Rectangle ball = new Rectangle(BALL_START_X, BALL_START_Y, BALL_DIAMETER,
+			BALL_DIAMETER);
 
 	Rectangle paddle1 = new Rectangle(PADDLE1_START_X, PADDLE_START_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
 	Rectangle paddle2 = new Rectangle(PADDLE2_START_X, PADDLE_START_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
 
 	Rectangle screen = new Rectangle(0, 0, WIDTH, HEIGHT);
 
-	//swing timer with a delay of 20ms and the Game object's actionListener
+	// swing timer with a delay of 20ms and the Game object's actionListener
 	Timer timer = new Timer(20, this);
 
-	//array to store key presses for paddle controls 
+	// array to store key presses for paddle controls
 	boolean[] keys = new boolean[256];
 
+	/*
+	 * This is the constructor for the Game class. As game extends JPanel, this
+	 * method can use the this keyword to set the background colour of the game,
+	 * and set the size based on the constant class variables defined earlier
+	 * (WIDTH and HEIGHT). This constructor also makes a new Frame for the game,
+	 * to replace the menu frame, and configures it correctly. This method also
+	 * adds a keylistener to the frame, so the program listens for keypresses
+	 * and adds them to the keys array. Finally the method starts the game timer
+	 * so that the actionlistener will allow for key input to be added to the
+	 * keys array.
+	 */
 	public Game() {
-		
+
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.setBackground(Color.black);
-		
+
 		JFrame frame = new JFrame("Pong");
 		frame.setResizable(false);
 		frame.setContentPane(this);
@@ -84,18 +97,29 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		this.timer.start();
 
 	}
 
-	public Clip audio() throws Exception {
+	/*
+	 * This method controls the audio for Pong - it's separated into a different
+	 * method to allow for easy setup of the audio, and for easy use of the
+	 * audio file where it's needed, in the update method. Method has a lot of
+	 * error protection - it can throw an exception if anything goes wrong, and
+	 * has a try catch block to catch an error with the audio file (most likely
+	 * caused by the file not existing). The audio file is a beep sound stored
+	 * in a .wav file, in the bin folder (hence the use of this.getClass()). The
+	 * method returns the audio clip (which is the beep sound) if no errors are
+	 * thrown, so that the method can be called elsewhere to allow for properly
+	 * encapsulated use of the audio file.
+	 */
+	private Clip audio() throws Exception {
 
 		Clip clip = AudioSystem.getClip();
 		try {
 
-			AudioInputStream ais = AudioSystem.getAudioInputStream(this
-					.getClass().getResource("beep-07.wav"));
+			AudioInputStream ais = AudioSystem.getAudioInputStream(this.getClass().getResource("beep-07.wav"));
 			clip.open(ais);
 
 		} catch (Exception e) {
@@ -106,7 +130,13 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
-	public void reset() {
+	/*
+	 * This method performs a simple job - it resets the position of the ball
+	 * and the paddles on both the x and y axes, and then picks a random
+	 * direction for the ball to travel in, by modifying the velocity of the
+	 * ball.
+	 */
+	private void reset() {
 
 		ball.x = BALL_START_X;
 		ball.y = BALL_START_Y;
@@ -127,7 +157,29 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
-	public void update() throws Exception {
+	/*
+	 * The update method is the longest, and also the most important method in
+	 * the code. It has to also be able to throw an exception, as it uses the
+	 * audio method from earlier. Firstly, it checks whether the game is paused
+	 * - if so, it returns control to where the update method is called (in the
+	 * actionPerformed method). If the game is playing and not paused, the keys
+	 * array is checked for up and down keys, or the w and s keys. The paddles
+	 * are then moved accordingly. The method then adds the ball's velocity to
+	 * the ball's position to keep it moving. The method then checks for
+	 * collision between ball and paddle, or paddle and wall, using the
+	 * collision rectangles defined earlier. If the ball collides with a paddle,
+	 * then it's velocity is not simply reversed but multiplied by a random
+	 * number, so as to make the ball deflect at a different angle and velocity
+	 * to the way it collided with the paddle. The audio method is called,
+	 * playing the clip once using the loop method with a parameter of 0. If the
+	 * ball hits the edge, it inverts the direction of the velocity of the ball,
+	 * so it appears to bounce off. If the ball goes off the left or right edge
+	 * without hitting a paddle, the score of the opposite player is
+	 * incremented, and the reset method is called to replace the paddles and
+	 * ball in their original positions. The game state is changed to game ended
+	 * so the game pauses and displays the score.
+	 */
+	private void update() throws Exception {
 
 		if (gameState != GAME_PLAYING) {
 			return;
@@ -225,6 +277,17 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	/*
+	 * This method casts an existing Graphics object into a new instance of the
+	 * Graphics2D class. It then calls the method paintComponent as the JPanel,
+	 * using the super keyword to do this. The method then calls a series of
+	 * methods from the Graphics2D class, and makes some new Objects, using the
+	 * methods of the Graphics2D class to change key details, like making the
+	 * line dashed with setStroke. If the game is paused, the method displays a
+	 * message, depending on whether the game has ended or has been paused mid
+	 * game with the space key.
+	 */
+	
 	public void paintComponent(Graphics g) {
 
 		Graphics2D g2d = (Graphics2D) g;
@@ -264,6 +327,12 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	/*
+	 * Method from the ActionListener interface. When the timer updates, the if
+	 * statement evaluates to true and runs the update method, and then calls
+	 * repaint, updating the graphics. In practice this runs on the delay set by
+	 * the timer, and ensures smooth graphics without screen flashing.
+	 */
 	public void actionPerformed(ActionEvent event) {
 
 		if (event.getSource() == timer) {
@@ -277,6 +346,8 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	// checks for keypresses, adds keypresses to array of keys. Pauses on space
+	// key press.
 	public void keyPressed(KeyEvent e) {
 
 		keys[e.getKeyCode()] = true;
@@ -293,12 +364,16 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	// stops adding keys to array when key is released, preventing paddle from
+	// keeping moving when key is not held down.
 	public void keyReleased(KeyEvent e) {
 
 		keys[e.getKeyCode()] = false;
 
 	}
 
+	// keytyped is just here because KeyListener is an interface, and so all
+	// it's methods need to be implemented.
 	public void keyTyped(KeyEvent e) {
 	}
 
--- PongMK5000/src/game/Window.java
+++ PongMK5000/src/game/Window.java
@@ -9,41 +9,54 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/* extends JFrame to allow for less static referencing - no point creating 
+ a new named instance of jFrame for the sake of static referencing when 
+jFrame could just be inherited. */
+
 public class Window extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	JButton playButton = new JButton("Play");
 	JButton exitButton = new JButton("Exit");
 
+	/* 640 x 480 dimensions picked because they're large enough to play a
+	 reasonable game, small enough to look retro.
+ 	Plus, large dimensions don't benefit primitive graphics. */
 	private int windowWidth = 640;
 	private int windowHeight = 480;
 
+	//main method for the whole program - makes a new instance of the Window class. 
 	public static void main(String[] args) {
 
 		new Window();
 
 	}
-	
+
+	//constructor for the window class, ran when a new instance of Window is created.
 	public Window() {
 
-		setResizable(false);
-		setSize(windowWidth, windowHeight);
-		setLocationRelativeTo(null);
-		setTitle("This version of Pong is still better than yours.");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		// creating basic window of the right size, setting title etc
+		this.setResizable(false);
+		this.setSize(windowWidth, windowHeight);
+		this.setLocationRelativeTo(null);
+		this.setTitle("This version of Pong is still better than yours.");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//make and configure the menu panel.
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setOpaque(false);
 		panel.setSize(windowWidth, windowHeight);
 		getContentPane().add(panel);
-		
+
+		// adding background image for the menu
 		ImageIcon icon;
 		JLabel picture;
 		icon = new ImageIcon(this.getClass().getResource("Pongmenu.jpg"));
 		picture = new JLabel(new ImageIcon(icon.getImage()));
-		add(picture);
+		this.add(picture);
 
+		// making the menu buttons
 		playButton.setBounds((windowWidth / 2) - 50, (windowHeight / 2) - 30, 100, 30);
 		playButton.setText("Play");
 		playButton.setFocusable(false);
@@ -53,26 +66,28 @@ public class Window extends JFrame implements ActionListener {
 		exitButton.setText("Exit");
 		exitButton.setFocusable(false);
 		panel.add(exitButton);
-
-		setVisible(true);
+		
+		this.setVisible(true);
 
 		playButton.addActionListener(this);
 		exitButton.addActionListener(this);
 
 	}
 
+	/* Method from the ActionListener interface, checks for user clicking on the play or quit buttons, gets rid of the menu frame
+	 * on play button click and then makes a new Game instance. */
 	public void actionPerformed(ActionEvent e) {
+
+		// if the play button is clicked, start a new game, get rid of the menu
 		if (e.getSource() == playButton) {
 
 			this.dispose();
 			new Game();
-
+			// if the exit button's clicked, call System.exit to terminate the program.
 		} else if (e.getSource() == exitButton) {
 			System.exit(0);
 		}
 
 	}
-
-	
 
 }
